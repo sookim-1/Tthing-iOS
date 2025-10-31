d71164434cc4882e8d918aa0570b37d93e6a94bb
sookim-1 <scstnghks@gmail.com>
2025-10-31 22:10:30 +0900

    build : xcode project 추가

---
+++ Tthing/Tthing/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Tthing
+//
+//  Created by sookim on 10/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Tthing/Tthing/TthingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TthingApp.swift
+//  Tthing
+//
+//  Created by sookim on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct TthingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
